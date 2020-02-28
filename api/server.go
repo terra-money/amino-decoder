5592016d1ac035803d9bcd23ee6367094bf29d0c
Yun <yun@terra.money>
2020-02-29 02:05:48 +0900

    change config to use terra prefix

--- api/server.go
+++ api/server.go
@@ -4,14 +4,37 @@ import (
 	"github.com/gorilla/mux"
 
 	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/terra-project/core/app"
 )
 
 var cdc *codec.Codec
 
+const (
+	// Bech32PrefixAccAddr defines the Bech32 prefix of an account's address
+	Bech32PrefixAccAddr = "terra"
+	// Bech32PrefixAccPub defines the Bech32 prefix of an account's public key
+	Bech32PrefixAccPub = "terrapub"
+	// Bech32PrefixValAddr defines the Bech32 prefix of a validator's operator address
+	Bech32PrefixValAddr = "terravaloper"
+	// Bech32PrefixValPub defines the Bech32 prefix of a validator's operator public key
+	Bech32PrefixValPub = "terravaloperpub"
+	// Bech32PrefixConsAddr defines the Bech32 prefix of a consensus node address
+	Bech32PrefixConsAddr = "terravalcons"
+	// Bech32PrefixConsPub defines the Bech32 prefix of a consensus node public key
+	Bech32PrefixConsPub = "terravalconspub"
+)
+
 func init() {
 	cdc = app.MakeCodec()
+	config := sdk.GetConfig()
+	config.SetCoinType(330)
+	config.SetFullFundraiserPath("44'/330'/0'/0/0")
+	config.SetBech32PrefixForAccount(Bech32PrefixAccAddr, Bech32PrefixAccPub)
+	config.SetBech32PrefixForValidator(Bech32PrefixValAddr, Bech32PrefixValPub)
+	config.SetBech32PrefixForConsensusNode(Bech32PrefixConsAddr, Bech32PrefixConsPub)
+	config.Seal()
 }
 
 // Server represents the API server
